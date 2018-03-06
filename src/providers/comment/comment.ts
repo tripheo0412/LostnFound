093b5e4ce54641e86cdab8df2f3c6858ed967c44
Minh Tri Hoang <tripheo0412@gmail.com>
2018-03-06 11:18:01 +0200

    comment provider added

--- src/providers/comment/comment.ts
+++ src/providers/comment/comment.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Api} from "../api/api";
 
 /*
   Generated class for the CommentProvider provider.
@@ -10,8 +11,37 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CommentProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(public api: Api) {
     console.log('Hello CommentProvider Provider');
   }
+  deleteComment(id){
+    this.api.delete('comments/'+id,this.api.settings).subscribe(res => {
+        console.log(res);
+      }, err => {
+        console.log(err);
+      }
+    )
+  }
 
+  postComment(fileId, comment) {
+    const body = {
+      file_id: fileId,
+      comment: comment
+    }
+    this.api.post('comments',body,this.api.settings).subscribe(res => {
+        console.log(res);
+      }, err => {
+        console.log(err);
+      }
+    )
+  }
+
+  listCommentByFile(id) {
+    this.api.get('comments/file/'+id,this.api.settings).subscribe(res => {
+        console.log(res);
+      }, err => {
+        console.log(err);
+      }
+    )
+  }
 }
