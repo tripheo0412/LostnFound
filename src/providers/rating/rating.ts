b518841c0cf33f6a44651a3620a6fb7b1476916e
Minh Tri Hoang <tripheo0412@gmail.com>
2018-03-06 11:04:32 +0200

    tag provider finished

--- src/providers/rating/rating.ts
+++ src/providers/rating/rating.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Api} from "../api/api";
 
 /*
   Generated class for the RatingProvider provider.
@@ -10,8 +11,22 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RatingProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(public api: Api) {
     console.log('Hello RatingProvider Provider');
   }
+  newRating(fileId, rating){
+    const body = {
+      file_id: fileId,
+      rating: rating
+    }
+    this.api.post('ratings',body,this.api.settings).subscribe(res => {
+        console.log(res);
+      }, err => {
+        console.log(err);
+      }
+    )
+  }
+
+
 
 }
