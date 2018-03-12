009a839ecc1910e0ce24395d75a93837bbfe2a96
Minh Tri Hoang <tripheo0412@gmail.com>
2018-03-13 01:43:50 +0200

    fix card data no sleeping for 3 days in a row im a herogit add .!

--- src/providers/market/market.ts
+++ src/providers/market/market.ts
@@ -35,6 +35,21 @@ export class MarketProvider {
         let start1 = resp.title.indexOf('location') + 8;
         let end1 = resp.title.indexOf('*year');
         let location = resp.title.slice(start1,end1);
+        let yearstart = resp.title.indexOf('yearfrom') +8;
+        let yearend= resp.title.indexOf('*yearto');
+        let year = resp.title.slice(yearstart,yearend);
+        let yearfrom = year.slice(0,4);
+        let yearto = year.slice(5,9);
+        let monthstart = resp.title.indexOf('monthfrom') +9;
+        let monthend= resp.title.indexOf('*monthto');
+        let month = resp.title.slice(monthstart,monthend);
+        let monthfrom = month.slice(0,2);
+        let monthto = month.slice(3,5);
+        let daystart = resp.title.indexOf('dayfrom') +7;
+        let dayend= resp.title.indexOf('*dayto');
+        let day = resp.title.slice(daystart,dayend);
+        let dayfrom = day.slice(0,2);
+        let dayto = day.slice(3,5);
         let content = resp.description;
         let time = resp.time_added;
         this.http.get(this.apiUrl+'/users/'+resp.user_id,settings).toPromise().then((resp: any) => {
@@ -57,7 +72,10 @@ export class MarketProvider {
               image: image,
               content: content,
               type: type,
-              location: location
+              location: location,
+              src: 'assets/icon/cardicon/'+type+'-icon.png',
+              datefrom: yearfrom+'-'+monthfrom+'-'+dayfrom ,
+              dateto: yearto+'-'+monthto+'-'+dayto
             }
             console.log(drawCard);
             this.finish.push(drawCard);
